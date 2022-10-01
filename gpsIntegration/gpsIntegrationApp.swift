6a22a5891f419219054e084a61eac933a2307b90
Amit Gupta <amit@gprof.com>
2022-10-01 14:26:26 -0700

    Initial Commit

---
+++ gpsIntegration/gpsIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpsIntegrationApp.swift
+//  gpsIntegration
+//
+//  Created by Amit Gupta on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct gpsIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
